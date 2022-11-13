f29502bf139b367c36b55bb7ab66a4be3f18b9e8
Devindi Jayawardena <devindijayawardena.dj@gmail.com>
2022-11-13 12:53:44 +0530

    Updated the button text

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                     .padding()
             }
             
-            Button("Click Me!") {
+            Button("Click To Proceed!") {
                 isTextShowing.toggle()
             }
         }
